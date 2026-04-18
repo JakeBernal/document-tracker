e406ae83d63216f9f0b07bdf7f0423a72106a8c2
jek <jakebernal512@gmail.com>
2026-04-18 16:31:24 +0800

    a

--- tracker/src/App.jsx
+++ tracker/src/App.jsx
@@ -1,15 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Signin from "./pages/signin";
 import Home from "./pages/home";
-<<<<<<< HEAD
 import Signup from "./pages/signup";
-import RequestForm from "./pages/RequestForm";
-import Admin from "./pages/admin";
-import Citizen from "./pages/citizen";
-import AdminRoute from "./routes/AdminRoute";
-=======
-import Signup from "./pages/signup"
->>>>>>> parent of 15092b3 (rolebasedaccess)
 
 function App() {
   return (
@@ -17,25 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
-<<<<<<< HEAD
         <Route path="/signup" element={<Signup />} />
-        <Route path="/request" element={<RequestForm />} />
-
-        {/* Citizen page */}
-        <Route path="/citizen" element={<Citizen />} />
-
-        {/* Admin protected route */}
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <Admin />
-            </AdminRoute>
-          }
-        />
-=======
-        <Route path="/signup" element={<Signup />} /> 
->>>>>>> parent of 15092b3 (rolebasedaccess)
       </Routes>
     </>
   );
